56991fd05e2e5b7477b8eb70b6a56a03f99f042f
maria-S-456 <mars381pika@gmail.com>
2017-01-01 19:19:54 -0500

    first commit

---
+++ js-nasa-picture.js
@@ -0,0 +1,35 @@
+var searchApiUrl = 'https://api.nasa.gov/planetary/apod';
+var apiKey = 'EhG1k6n5owkDLwymr8VLuyohnQbd4xYK3bUigawl';
+
+
+function searchResults(data) {
+  var results = $('.content-div');
+  results.empty();
+  var imageLink = data.url;
+  var heading = data.title;
+  var descp =data.explanation;
+  var date= data.date;
+  var elem = '<div id="picture-div"><img src="'+ imageLink +'" /></a></div><div id="text-div"><h2> '+ heading +' </h2><p> '+ descp +' </p></div>';
+  results.append(elem);
+}
+
+function searchNasa(searchText, handleResultsFn) {
+  var data = {
+  api_key: apiKey,
+  date: searchText,
+  }
+
+  $.getJSON(searchApiUrl, data, handleResultsFn);
+}
+
+function handleSearchSubmit() {
+  $('#input-form').submit(function(event){
+  event.preventDefault();
+  var searchText = $(event.currentTarget).find('.input-box').val().trim();
+  searchNasa(searchText, searchResults);
+  });
+}
+
+$(document).ready(function() {
+  handleSearchSubmit();
+});
